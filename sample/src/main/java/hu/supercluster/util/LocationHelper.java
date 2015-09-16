167e93979e5fc905ba90a5dd1d1079a224c669e5
zzss <zsolt.kocsi@gmail.com>
2015-09-16 19:24:34 +0200

    Add LocationHelper

---
+++ sample/src/main/java/hu/supercluster/util/LocationHelper.java
@@ -0,0 +1,27 @@
+package hu.supercluster.util;
+
+import android.location.Location;
+import android.location.LocationManager;
+
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.SystemService;
+
+@EBean
+public class LocationHelper {
+    @SystemService
+    LocationManager locationManager;
+
+    public Location getLastKnownLocation() {
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location == null) {
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+
+        if (location == null) {
+            location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        }
+
+        return location;
+    }
+}
