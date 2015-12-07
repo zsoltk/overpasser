1073c3ffb373afaf1ae8333efb7eb1cefa6f3f1c
zzss <zsolt.kocsi@gmail.com>
2015-12-07 18:32:31 +0100

    Fix indentation

--- library/src/test/java/hu/supercluster/overpasser/library/query/OverpassQueryTest.java
+++ library/src/test/java/hu/supercluster/overpasser/library/query/OverpassQueryTest.java
@@ -68,13 +68,13 @@ public class OverpassQueryTest {
                 .format(JSON)
                 .timeout(30)
                 .filterQuery()
-                .node()
-                .amenity("parking")
-                .notEquals("access", "private")
-                .boundingBox(
-                        47.48047027491862, 19.039797484874725,
-                        47.51331674014172, 19.07404761761427
-                )
+                    .node()
+                    .amenity("parking")
+                    .notEquals("access", "private")
+                    .boundingBox(
+                            47.48047027491862, 19.039797484874725,
+                            47.51331674014172, 19.07404761761427
+                    )
                 .end()
                 .output(OutputVerbosity.BODY, OutputModificator.CENTER, OutputOrder.QT, 100)
                 .build()
@@ -91,17 +91,17 @@ public class OverpassQueryTest {
                 .format(JSON)
                 .timeout(30)
                 .filterQuery()
-                  .way()
-                  .amenity("parking")
-                  .notEquals("access", "private")
-                  .boundingBox(
+                    .way()
+                    .amenity("parking")
+                    .notEquals("access", "private")
+                    .boundingBox(
                           47.48047027491862, 19.039797484874725,
                           47.51331674014172, 19.07404761761427
-                  )
+                    )
                 .end()
                 .output(OutputVerbosity.BODY, OutputModificator.CENTER, OutputOrder.QT, 100)
                 .build()
-                ;
+        ;
 
         String expected = "[\"out\":\"json\"][\"timeout\":\"30\"]; (way[\"amenity\"=\"parking\"][\"access\"!=\"private\"](47.48047027491862,19.039797484874725,47.51331674014172,19.07404761761427);<;); out body center qt 100;";
 
